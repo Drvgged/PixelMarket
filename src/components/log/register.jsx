0cc1150ad743b98aa27d46164c99690cc0f6c022
fabriguaglia <fabriguaglia72@gmail.com>
2023-08-10 15:37:49 -0300

    login-and-register

---
+++ src/components/log/register.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import './register.css';
+
+const Register = () => {
+  return (
+    <body>
+    <div className="principal">
+      <h1>Crea tu cuenta</h1>
+      <form>
+      <div className="inputBox">
+          <input
+            type="email"
+            name="email"
+            required
+          />
+          <label>Correo</label>
+        </div>
+        <div className="inputBox">
+          <input
+            type="text"
+            name="text"
+            required
+          />
+          <label>Usuario</label>
+        </div>
+        <div className="inputBox">
+          <input
+            type="password"
+            name="password"
+            required
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+          />
+          <label>Contraseña</label>
+        </div>
+        <div className='termycond'>
+        <label>
+            Acepto los <a className='acepterm' href="terminos-y-condiciones">Términos y Condiciones</a>
+            <input type="checkbox" className='check'/>
+          </label>
+        </div>
+        <input type="submit" name="sign-in" value="Iniciar sesión" />
+      </form>
+    </div>
+    </body>
+  );
+};
+
+export default Register;
