5a9ce0b3231dd4680ac10bcd10b0e26d100a5776
Agussortiz <aguusortiz04@gmail.com>
2023-08-16 18:37:28 -0300

    product

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
+import Footer from './components/footer/footer.jsx';
+import Header from './components/header/header';
+import Home from './components/home/home';
+import Products from './components/products/products';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/products/products.jsx
@@ -0,0 +1,282 @@
+import React from 'react'
+import spritesImport from "../../assets/sprites/spritesImports";
+import './products.css';
+import OwnProducts from './products-reutilizable';
+
+function Products() {
+  return (
+    <>
+            <div className="prod">
+            <div className="divider1"><hr></hr></div>
+                <div className="box">
+                    <h2>Latest Featured Pixels</h2>
+                    <div className='prod-box-princ'>
+                        <div className='boxxing'>
+                            <a href="/products/among-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.amongSprite }
+                                    title="AMONG US SPRITE"
+                                    price="$4.99"
+                                    
+                                    info="A pixel art of Among Us, my original creation"
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+                            <a href="/products/other-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.foxSprite }
+                                    title='Other Sprite'
+                                    price='$4.99'
+                                    info='A pixel art of Other, my original creation'
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+                            <a href="/products/gangster-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.gangsterSprite }
+                                    title="Gangster Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of a Gangster, my original creation"
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+                            <a href="/products/lario-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.larioSprite }
+                                    title='Lario Sprite'
+                                    price='$4.99'
+                                    info='A pixel art of Lario, my original creation'
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+                            <a href="/products/warrior-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.warriorSprite }
+                                    title="Warrior Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of a Woman Warrior, my original creation"
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+                            <a href="/products/among-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.amongSprite }
+                                    title="Among Us Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of Among Us, my original creation"
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+                            <a href="/products/among-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.amongSprite }
+                                    title="Among Us Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of Among Us, my original creation"
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+                            <a href="/products/among-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.amongSprite }
+                                    title="Among Us Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of Among Us, my original creation"
+                                />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className='box'>
+                    <h2>Fresh Pixels</h2>
+                    <div className='prod-box-2'>
+                        <div className='boxxing'>
+                            <a href="/products/warrior-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.warriorSprite }
+                                    title="Warrior Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of a Woman Warrior, my original creation"
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+                            <a href="/products/warrior-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.warriorSprite }
+                                    title="Warrior Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of a Woman Warrior, my original creation"
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+                            <a href="/products/warrior-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.warriorSprite }
+                                    title="Warrior Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of a Woman Warrior, my original creation"
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+
+                            <a href="/products/warrior-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.warriorSprite }
+                                    title="Warrior Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of a Woman Warrior, my original creation"
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+
+                            <a href="/products/warrior-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.warriorSprite }
+                                    title="Warrior Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of a Woman Warrior, my original creation"
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+
+                            <a href="/products/warrior-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.warriorSprite }
+                                    title="Warrior Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of a Woman Warrior, my original creation"
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+
+                            <a href="/products/warrior-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.warriorSprite }
+                                    title="Warrior Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of a Woman Warrior, my original creation"
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+
+                            <a href="/products/warrior-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.warriorSprite }
+                                    title="Warrior Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of a Woman Warrior, my original creation"
+                                />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className='box'>
+                    <h2>Featured jams</h2>
+                    <div className='prod-box-2'>
+                        <div className='boxxing'>
+                            <a href="/products/warrior-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.warriorSprite }
+                                    title="Warrior Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of a Woman Warrior, my original creation"
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+                            <a href="/products/warrior-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.warriorSprite }
+                                    title="Warrior Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of a Woman Warrior, my original creation"
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+                            <a href="/products/warrior-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.warriorSprite }
+                                    title="Warrior Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of a Woman Warrior, my original creation"
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+
+                            <a href="/products/warrior-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.warriorSprite }
+                                    title="Warrior Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of a Woman Warrior, my original creation"
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+
+                            <a href="/products/warrior-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.warriorSprite }
+                                    title="Warrior Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of a Woman Warrior, my original creation"
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+
+                            <a href="/products/warrior-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.warriorSprite }
+                                    title="Warrior Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of a Woman Warrior, my original creation"
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+
+                            <a href="/products/warrior-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.warriorSprite }
+                                    title="Warrior Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of a Woman Warrior, my original creation"
+                                />
+                            </a>
+                        </div>
+                        <div className='boxxing'>
+
+                            <a href="/products/warrior-sprite">
+                                <OwnProducts
+                                    img={ spritesImport.warriorSprite }
+                                    title="Warrior Sprite"
+                                    price="$4.99"
+                                    info="A pixel art of a Woman Warrior, my original creation"
+                                />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+  )
+}
+
+export default Products
